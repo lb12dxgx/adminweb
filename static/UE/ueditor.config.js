9fef6de75d142ff90e5e49e59457a27b58596b21
lb1213 <zhanggy@weeon.cn>
2018-05-07 18:13:06 +0800

    fff

--- static/UE/ueditor.config.js
+++ static/UE/ueditor.config.js
@@ -32,8 +32,8 @@
         UEDITOR_HOME_URL: "/static/UE/" 
 
         // 服务器统一请求接口路径
-        , serverUrl: "http://127.0.0.1:8080/" +  "exec"
-       //, serverUrl: "http://120.79.20.2:8080/" +  "exec"
+        //, serverUrl: "http://127.0.0.1:8080/" +  "exec"
+       , serverUrl: "http://120.79.20.2:8080/" +  "exec"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         , toolbars: [[
