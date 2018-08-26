f5d5ff320e6a40b22f6220f55aa9c0312d4cc50b
lb12dxgx <38183832+lb12dxgx@users.noreply.github.com>
2018-08-26 20:08:08 +0800

    完善企业查询

--- src/api/head.js
+++ src/api/head.js
@@ -5,6 +5,7 @@ const mockAxios = Axios.create();
 const mock = new axiosMock(mockAxios);
 const uuidv4 = require('uuid/v4');
 //const ip="http://127.0.0.1"
-const ip="http://127.0.0.1"
+const ip="http://120.79.20.2"
 mock.restore();//去掉模拟行为
 export  {mock,mockAxios,router,uuidv4,ip}
+ 
